85c2dc92b654193c1573b7997fc72132d3ae7638
tcsigmon101 <thomascsigmon1@gmail.com>
2021-03-05 11:18:13 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tuner from "./components/tuner";
+import { Store } from "./store";
+import { Provider } from "react-redux";
+
+import { MainContainer } from "./global.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={Store}>
+      <MainContainer className="App">
+        <Tuner />
+      </MainContainer>
+    </Provider>
   );
 }
 
